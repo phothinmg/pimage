ae2a22ea9a98f65dfba084564116afd0fd9b9634
phothinmg <phothinmg@gmail.com>
2024-07-23 06:52:53 +0630

    modified:   src/index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
 export * from "./image-to-uri/index.js";
 export * from "./transform/index.js";
 export * from "./_transform.js";
-export * from "@imagemagick/magick-wasm";
-export * from "./magick/_m_type.js";
+
--- src/magick/_m_enum.ts
+++
@@ -1,9 +0,0 @@
-import {
-  MagickFormat,
-  Gravity,
-  PaintMethod,
-  TextAlignment,
-  TextDecoration,
-} from "@imagemagick/magick-wasm";
-
-export { MagickFormat, Gravity, PaintMethod, TextAlignment, TextDecoration };
